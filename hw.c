ee2c720c26590340896f82fcb9e4b651154ca527
Javier Martínez Albaladejo <jma104@alu.ua.es>
2020-12-26 14:09:22 +0100

    Added pre-commit hook

--- hw.c
+++ hw.c
@@ -4,5 +4,6 @@ int main() {
     printf("Hola, git!\n");
     printf("Buenos días\n");
     printf("Esto es un proyecto de prueba\n");
+    printf("Hago un cambio para probar el hook pre-commit\n");
     return 0;
 }
